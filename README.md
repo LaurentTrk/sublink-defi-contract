4098eebe77a139c0c39a7e97504e04b06c478128
Laurent Turek <laurent.turek@gmail.com>
2022-05-23 18:06:36 +0200

    Simpliest contract using SubLink extension

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# sublink-defi-contract
+# SubLink Defi Contract
+
 A very simple contract to demonstrate how to get price feed from SubLink in an ink! contract
+
+To use this contract, your parachain needs to use a couple of SubLink pallets, as explained in the main [SubLink repository](https://github.com/LaurentTrk/sublink).
+
--- lib.rs
+++ lib.rs
@@ -2,61 +2,31 @@
 
 use ink_lang as ink;
 
+// We use the SubLink environment that provides us the SubLink ink! extension
 #[ink::contract(env = sublink_ink_contract_extension::SubLinkEnvironment)]
 mod sublink_defi_contract {
+
+    use sublink_ink_contract_extension::RoundData;
 
     
     #[ink(storage)]
     pub struct SubLinkDefiContract {
-        /// We store the latest value retrieve from the extension
-        latest_price_value: u128
     }
 
-    #[ink(event)]
-    pub struct FeedUpdated {
-        #[ink(topic)]
-        feed_id: u32,
-        answer: u128,
-    }
 
     impl SubLinkDefiContract {
         // Instantiate a new contract
         #[ink(constructor)]
         pub fn new() -> Self {
-            Self { latest_price_value: Default::default() }
+            Self { }
         }
-
-        /// Get the latest price value of a given feed.
-        /// Will call the SubLink ink! extension
-        #[ink(message)]
-        pub fn latest_data(&mut self, feed_id: u32) -> Result<u128, sublink_ink_contract_extension::SubLinkErr> {
-            let caller = self.env().caller();
-            let message = ink_prelude::format!("got a call from {:?}", caller);
-            ink_env::debug_println!("{}", &message);
-         
-            log::info!("called latest_data contract");
-            let feed = self.env().extension().latest_data(feed_id)?;
-            ink_env::debug_println!("############ Received value {:?}", &feed);
-            
-            self.latest_price_value = feed;
-
-            self.env().emit_event(FeedUpdated { feed_id: feed_id, answer: feed });
-            Ok(feed)
-        }
-
-        /// Return the latest value received from the SubLink ink! extension
-        #[ink(message)]
-        pub fn latest_price_value(&self) -> u128 {
-            self.latest_price_value
-        }        
         
-        /// Return the latest value received from the SubLink ink! extension
+        /// Return the latest round data received from the SubLink ink! extension
         #[ink(message)]
-        pub fn get_latest_price(&self, feed_id: u32) -> u128 {
-            let latest_price = self.env().extension().latest_data(feed_id).unwrap_or_default();
-            ink_env::debug_println!("######################## get_latest_price value {:?}", &latest_price);
-            latest_price
-            
+        pub fn get_latest_round_data(&self, feed_id: u32) -> RoundData {
+            // The extension gives us the latest_round_data function
+            // (as defined in Chainlink API https://docs.chain.link/docs/price-feeds-api-reference/#latestrounddata)
+            self.env().extension().latest_round_data(feed_id).unwrap_or_default()
         }
     }
 }
